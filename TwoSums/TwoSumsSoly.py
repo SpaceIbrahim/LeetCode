129b0ee3b986863237e99424f00c3fb176fd156e
SpaceIbrahim <ibrahimm4566@gmail.com>
2023-09-26 20:32:09 -0400

    Two sums solution

--- TwoSums/TwoSumsSoly.py
+++ TwoSums/TwoSumsSoly.py
@@ -1,1 +1,23 @@
-print("POOP")
+
+def twoSum(nums, target):
+    map = {}
+    output = []
+    for i in range(0, len(nums)):
+        
+        if (target - nums[i]) in map.values():
+            
+            output = [i, list(map.values()).index(target - nums[i])]
+        else:
+            map[i] = nums[i]
+    return output
+
+def main(args=None):
+    nums = [2,7,11,15]
+    target = 9
+    output = twoSum(nums, target)
+
+    print(output)
+
+if __name__ == '__main__':
+    main()
+        
