b5ea309b72b3aefd4edcf72b5bc39849247237d4
SpaceIbrahim <ibrahimm4566@gmail.com>
2023-09-28 19:04:01 -0400

    IsAnagram Problem

---
+++ IsAnagram/isanagram.py
@@ -0,0 +1,15 @@
+
+def isanagram(s, t):
+        
+        return True
+
+def main(args=None):
+    s = "anagram"
+    t = "nagaram"
+    output = isanagram(s, t)
+
+    print(output)
+
+if __name__ == '__main__':
+    main()
+        
