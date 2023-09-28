710270588b80646aec1114502c7e516b882fea92
SpaceIbrahim <ibrahimm4566@gmail.com>
2023-09-28 19:34:28 -0400

    Epic sol 98% faster

--- IsAnagram/isanagram.py
+++ IsAnagram/isanagram.py
@@ -1,11 +1,26 @@
 
 def isanagram(s, t):
+        map  = {}
+        if len(s) != len(t):
+            return False
         
+        for i in s:
+            if i not in map:
+                  map[i] = 1
+            else:
+                map[i] += 1
+        for i in t:
+            if i in map:
+                map[i] -= 1
+        
+        for val in map.values():
+             if val !=0:
+                  return False
         return True
 
 def main(args=None):
-    s = "anagram"
-    t = "nagaram"
+    s = "aacc"
+    t = "caccc"
     output = isanagram(s, t)
 
     print(output)
