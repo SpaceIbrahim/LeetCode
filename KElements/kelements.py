f50a29aeca5bde4f1c20ea8a7bbb86c00df2c474
SpaceIbrahim <ibrahimm4566@gmail.com>
2023-09-29 20:33:37 -0400

    Nothing

--- KElements/kelements.py
+++ KElements/kelements.py
@@ -1,22 +1,34 @@
 
 def kelements(nums, k):
         map = {}
-        numSet = set(nums)
+        map2 = {}
         output = []
-        k = 0
         for i in nums:
             if i not in map:
                 map[i] = 1
             else:
                 map[i] +=1
+        for i,j in map.items():
+             print(i)
+             print(j)
+             map2[i] = j
+        print(map2)
+        # for key,value in map.items():
+        #     map2[value] = key
+        #     print(f"map2[{value}] = {key}")
+        Sort = list(map2.keys())
+        Sort.sort(reverse=True)
 
-        for i in map:
-            output[k] = i
-            k+=1
-        return [1]
+        map = {map2[i]: i for i in Sort}
+
+        
+
+        for _ in range(k):
+            output.append(map2[Sort[_]])
+        return output
 
 def main(args=None):
-    nums = [1,1,1,2,2,3]
+    nums = [1,2]
     k = 2
     output = kelements(nums, k)
 
