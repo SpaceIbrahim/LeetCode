e794ae65c0f2898ee4114f3931c591bd2bcea16b
SpaceIbrahim <ibrahimm4566@gmail.com>
2023-10-02 17:44:34 -0400

    Valid Sudoku

---
+++ ValidSudoku/validsudoku.py
@@ -0,0 +1,14 @@
+
+def validsudoku(s):
+        
+        return True
+
+def main(args=None):
+    s = "Let's take LeetCode contest"
+    output = validsudoku(s)
+
+    print(output)
+
+if __name__ == '__main__':
+    main()
+        
