cc9e69546cc1979140f0f06cf7fd07a4a2c748f8
SpaceIbrahim <ibrahimm4566@gmail.com>
2023-10-02 11:21:20 -0400

    Oh wait, I am goated

--- ColorGame/colorgame.py
+++ ColorGame/colorgame.py
@@ -1,13 +1,34 @@
 
 def colorgame(colors):
-        print(colors)
-        for i in range(1, len(colors)-1):
-            print(colors[i])
+        # A = 0
+        # B = 0
+        # for i in range(1, len(colors)-1):
+        #     # print(colors[i - 1], " ", colors[i], " ", colors[i + 1])
+        #     if "AAA" in colors:
+        #         p = colors.find("AAA")+1
+        #         colors = colors[:p] + colors[p+1:]
+        #         A += 1
+        #     if "BBB" in colors:
+        #         p = colors.find("BBB")+1
+        #         colors = colors[:p] + colors[p+1:]
+        #         B += 1
         
-        return False
+        A = colors.count("AAA")
+        B = colors.count("BBB")
+        while A == B:
+            if "AAA" not in colors:
+             return False
+            colors = colors.replace('AAA', 'AA')
+            colors = colors.replace('BBB', 'BB')
+            A = colors.count("AAA")
+            B = colors.count("BBB")
+            print(A, " ", B, "       ", colors)
+
+        print(A, " ", B)
+        return A > B
 
 def main(args=None):
-    s = "AAABABB"
+    s = "AABB"
     output = colorgame(s)
 
     print(output)
