bb332084eea305ea52dc252170e0ceda1f812f3e
SpaceIbrahim <ibrahimm4566@gmail.com>
2023-10-03 21:04:00 -0400

    DOne

--- DesignHashMap/myhashmap.py
+++ DesignHashMap/myhashmap.py
@@ -2,6 +2,7 @@ class MyHashMap(object):
 
     def __init__(self):
         self.map = []
+        self.keys = []
 
     def put(self, key, value):
         """
@@ -9,8 +10,12 @@ class MyHashMap(object):
         :type value: int
         :rtype: None
         """
-        self.map.append([key, value])
-        print(self.map)
+        if key in self.keys:
+            self.map[self.keys.index(key)][1] = value
+        else:
+            self.keys.append(key)
+            self.map.append([key, value])
+        
         
 
     def get(self, key):
@@ -18,6 +23,10 @@ class MyHashMap(object):
         :type key: int
         :rtype: int
         """
+        if key not in self.keys:
+            return -1
+        return self.map[self.keys.index(key)][1]
+
         
 
     def remove(self, key):
@@ -25,8 +34,11 @@ class MyHashMap(object):
         :type key: int
         :rtype: None
         """
-
-
+        if key not in self.keys:
+            return
+        self.map.remove(self.map[self.keys.index(key)])
+        self.keys.remove(key)
+        
 def main(args=None):
     myHashMap =  MyHashMap()
     myHashMap.put(1, 1)
@@ -37,6 +49,11 @@ def main(args=None):
     myHashMap.get(2)
     myHashMap.remove(2)
     myHashMap.get(2)
+
+    # a = [[2,1],[5,3]]
+    # b = [x[0] for x in a]
+    # print(a)
+    # print(b)
 
 if __name__ == '__main__':
     main()
