377bb4fa1006fd19073f1459ba08d3f013885afa
SpaceIbrahim <ibrahimm4566@gmail.com>
2023-10-06 13:24:18 -0400

    Reverse Int

---
+++ ReverseInt/reverseint.py
@@ -0,0 +1,29 @@
+
+def reversestring(x):
+        
+        num = str(x)
+        output = ""
+
+        for i in num:
+             output = i + output
+
+        if "-" in output:
+            output = output[:len(num)-1]
+            output = '-' + output
+
+        output = int(output)
+        if output > (2**31-1):
+             return 0
+        elif output < -(2**31):
+             return 0
+        return output
+
+def main(args=None):
+    int = -123
+    output = reversestring(int)
+
+    print(output)
+
+if __name__ == '__main__':
+    main()
+        
